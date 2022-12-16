8825440a73e8ed529feb57867e7a27fa8a8942d3
JanaAndri <andrijana.sekulovska@gmail.com>
2022-12-16 15:10:12 +0100

    AN first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "san-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
